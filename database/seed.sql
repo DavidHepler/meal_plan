ad47ac693df44e092b65a36bd6040d0d04155c72
David Hepler <dhepler@netapp.com>
2025-10-26 21:07:58 -0500

    removed the recipes that were suggested

--- database/seed.sql
+++ database/seed.sql
@@ -1,26 +1,8 @@
 -- Sample meal data
 
 INSERT INTO meals (name, nationality, main_component, secondary_component, recipe_location) VALUES
-('Spaghetti Carbonara', 'Italian', 'Pork', 'Pasta', 'https://www.allrecipes.com/recipe/11973/spaghetti-carbonara-ii/'),
-('Chicken Tikka Masala', 'Indian', 'Chicken', 'Rice', 'https://www.bbcgoodfood.com/recipes/chicken-tikka-masala'),
-('Beef Tacos', 'Tex-Mex', 'Beef', 'Other', 'https://www.simplyrecipes.com/recipes/beef_tacos/'),
-('Schnitzel with Spätzle', 'German', 'Pork', 'Other Grain', 'https://www.thespruceeats.com/wiener-schnitzel-recipe-1447089'),
-('Grilled Salmon with Rice', 'Japanese', 'Fish', 'Rice', 'https://www.justonecookbook.com/grilled-fish/'),
-('Margherita Pizza', 'Italian', 'Vegetarian', 'Other', 'https://www.bbcgoodfood.com/recipes/margherita-pizza'),
-('Sauerbraten', 'German', 'Beef', 'Potato', 'https://www.thespruceeats.com/sauerbraten-recipe-1447283'),
-('Thai Green Curry', 'Thai', 'Chicken', 'Rice', 'https://www.bbcgoodfood.com/recipes/thai-green-curry'),
-('Fish and Chips', 'British', 'Fish', 'Potato', 'https://www.bbcgoodfood.com/recipes/classic-fish-chips'),
-('Vegetable Stir Fry', 'Chinese', 'Vegetarian', 'Rice', 'https://www.simplyrecipes.com/recipes/vegetable_stir_fry/'),
-('Bratwurst with Sauerkraut', 'German', 'Pork', 'Potato', 'https://www.thespruceeats.com/bratwurst-with-sauerkraut-1447074'),
-('Chicken Fajitas', 'Tex-Mex', 'Chicken', 'Other', 'https://www.allrecipes.com/recipe/76496/chicken-fajitas/'),
-('Pasta Primavera', 'Italian', 'Vegetarian', 'Pasta', 'https://www.allrecipes.com/recipe/223042/pasta-primavera/'),
-('Baked Cod with Potatoes', 'Scandinavian', 'Fish', 'Potato', 'https://www.bbcgoodfood.com/recipes/baked-cod-tomatoes'),
-('Beef Stroganoff', 'Russian', 'Beef', 'Pasta', 'https://www.allrecipes.com/recipe/16311/beef-stroganoff-iii/'),
-('Vegetarian Chili', 'Tex-Mex', 'Vegetarian', 'Rice', 'https://cookieandkate.com/vegetarian-chili-recipe/'),
-('Pork Schnitzel', 'German', 'Pork', 'Potato', 'https://www.thespruceeats.com/pork-schnitzel-recipe-1447256'),
-('Teriyaki Chicken', 'Japanese', 'Chicken', 'Rice', 'https://www.allrecipes.com/recipe/128532/teriyaki-chicken/'),
-('Lasagna', 'Italian', 'Beef', 'Pasta', 'https://www.allrecipes.com/recipe/23600/worlds-best-lasagna/'),
-('Grilled Trout', 'German', 'Fish', 'Potato', 'https://www.thespruceeats.com/grilled-trout-recipe-1447251');
+('Spaghetti Carbonara', 'Italian', 'Pork', 'Pasta', ''),
+('Flammkuchen', 'French', 'Beef', '', '');
 
 -- Sample meal plan for the current week (starting from next Monday)
 -- This will be populated with some initial data
